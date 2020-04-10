379b3088b02b3c64d5ead076d6ea09341c44daea
s19358 <s19358@pjwstk.edu.pl>
2020-04-10 14:11:28 +0200

    initial commit

---
+++ tutorial6/tutorial6/Models/Enrollment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace tutorial6.Models
+{
+    public class Enrollment
+
+    {
+        public int idEnrollment { get; set; }
+        public string studies { get; set; }
+        public int semester { get; set; }
+        public int idStudy { get; set; }
+    }
+}
---
+++ tutorial6/tutorial6/Models/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace tutorial6.Models
+{
+    public class Student
+    {
+        [Required]  //if user not provide index then it will return 400 Bad request
+        [RegularExpression("^s[0-9]+$")]
+        [MaxLength(100)]
+        public string IndexNumber { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string LastName { get; set; }
+
+        [Required]
+        public DateTime BirthDate { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string studies { get; set; }   //Computer Science
+    }
+}
